28b95a5e6e3471ac150ad8b4a81249973160eae1
StarTux <ths.loy@gmail.com>
2019-04-01 20:34:30 +0200

    Add back title support

--- src/main/java/com/winthier/chat/title/TitleHandler.java
+++ src/main/java/com/winthier/chat/title/TitleHandler.java
@@ -1,17 +1,15 @@
 package com.winthier.chat.title;
 
 import com.winthier.chat.Message;
-// import com.winthier.title.Title;
-// import com.winthier.title.TitlePlugin;
+import com.winthier.title.Title;
+import com.winthier.title.TitlePlugin;
 
-/* Zombiefied for Nukkit port */
 public final class TitleHandler {
     public void loadTitle(Message message) {
-        return;
-        // if (message.sender == null) return;
-        // Title title = TitlePlugin.getInstance().getPlayerTitle(message.sender);
-        // if (title == null) return;
-        // message.senderTitle = title.getTitle();
-        // message.senderTitleDescription = title.getDescription();
+        if (message.sender == null) return;
+        Title title = TitlePlugin.getInstance().getPlayerTitle(message.sender);
+        if (title == null) return;
+        message.senderTitle = title.getTitle();
+        message.senderTitleDescription = title.getDescription();
     }
 }
