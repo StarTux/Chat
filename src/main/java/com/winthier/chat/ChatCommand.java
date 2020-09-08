c7ff5a24f4dcec073d394cc5b1958538e7b6f473
StarTux <ths.loy@googlemail.com>
2020-09-08 18:16:51 +0200

    Avoid NPE

--- src/main/java/com/winthier/chat/ChatCommand.java
+++ src/main/java/com/winthier/chat/ChatCommand.java
@@ -64,7 +64,8 @@ public final class ChatCommand extends AbstractChatCommand {
             } else {
                 return false;
             }
-            if (channel == null || !channel.hasPermission(player)) return true;
+            if (channel == null) return true;
+            if (player != null && !channel.hasPermission(player)) return true;
             StringBuilder sb = new StringBuilder();
             int count = 0;
             for (Chatter chatter: channel.getOnlineMembers()) {
