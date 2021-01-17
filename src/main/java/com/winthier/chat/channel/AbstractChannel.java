268e126e0d290f25c35ef848edcbbb8f2a3e523c
StarTux <ths.loy@googlemail.com>
2021-01-17 23:55:00 +0100

    Move checks around

--- src/main/java/com/winthier/chat/channel/AbstractChannel.java
+++ src/main/java/com/winthier/chat/channel/AbstractChannel.java
@@ -302,15 +302,15 @@ public abstract class AbstractChannel implements Channel {
         if (player.hasPermission("chat.nocancel")) return false;
         message = message.strip();
         int len = message.length();
-        if (len <= 1) return true;
         char c = Character.toLowerCase(message.charAt(0));
         if (c >= '0' && c <= '9') return false; // any number
         if (c < 'a' || c > 'z') return false; // not a letter
         String nmessage = message.replace(" ", "");
         int nlen = nmessage.length();
-        if (nlen <= 4) {
+        if (nlen <= 3) {
             if (c == 'k' || c == 'g') return false; // gg or kk
         }
+        if (len <= 1) return true;
         for (int i = 1; i < len; i += 1) {
             char d = nmessage.charAt(i);
             if (c != d) return false;
